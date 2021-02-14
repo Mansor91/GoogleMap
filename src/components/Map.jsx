44f9306134d13e0d69e84bb059b17c4a0f93a7f6
Mansor Mansor <mansor.m000@gmail.com>
2021-02-14 15:57:56 +0200

    Add many markers

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -9,11 +9,11 @@ const MyGoogleMap = withScriptjs(
             defaultZoom={14}
                 defaultCenter={{ lat: 31.2519153, lng: 34.7602093}}
         >
-        <Marker key={"Marker1"}
+        <Marker key={1}
             position={{ lat: 31.2519153, lng: 34.7602093}}
-            onClick={(e) => props.selectMarker(e)}
+            onClick={() => props.selectMarker(1)}
         >
-        {props.show && (
+        {props.showArr[0].show && (
             <InfoWindow>
                 <div style={{width: 200, height: 200}}>
                     <h1>Phone store</h1>
@@ -23,25 +23,47 @@ const MyGoogleMap = withScriptjs(
             </InfoWindow>
         )}
         </Marker>
+
+        <Marker key={2}
+            position={{ lat: 30.2519153, lng: 37.7602093}}
+            onClick={() => props.selectMarker(2)}
+        >
+        {props.showArr[1].show && (
+            <InfoWindow>
+                <div style={{width: 200, height: 200}}>
+                    <h1>Phone store2</h1>
+                    <p>Be'er sheva 53208</p>
+                    <img src={img1} alt="" width="150"/>
+                </div>
+            </InfoWindow>
+        )}
+        </Marker>
+
         </GoogleMap>
         )
     )
 )
 
-
 export default class Map extends React.Component {
     constructor() {
         super();
         this.state = {
-            show : false,
-            selectedMarker : null
+            showArr : [{id:1, show:false},{id:2, show:false}]
         }
     }
-    selectMarker = (marker) => {
-        this.setState({ show : !this.state.show })
-        //console.log(marker)
-        //this.setState({ selectedMarker: event.target})
+
+    selectMarker = (id) => {
+       
+        let tmpArr = [...this.state.showArr]
+        for (const marker of tmpArr) {
+            if (id === marker.id) {
+                marker.show = !marker.show
+            }
+        }
+        this.setState({ showArr : tmpArr })
     }
+
+
     render() {
         return (
             <div>
@@ -51,71 +73,10 @@ export default class Map extends React.Component {
                     containerElement={<div style={{height: '100%'}} className="map"></div>}
                     mapElement={<div style={{height: '1000px'}} className="inner-map"></div>}
                     googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVtHhDa2ErB1UuzEc1t3dXozBGaSHiQZk&v=3.exp&libraries=geometry,drawing,places"
-                    show = {this.state.show}
+                    showArr = {this.state.showArr}
                     selectMarker = {this.selectMarker}
                 />
             </div>
         )
     }
 }
-
-
-
-
-// const Marker = props => (
-//     <React.Fragment>
-//       <div
-//         style={{
-//           border: "5px solid white",
-//           borderRadius: 20,
-//           height: 20,
-//           width: 20
-//         }}
-//       />
-//       {/* Below is info window component */}
-//       {props.show && (
-//         <div
-//           style={{
-//             width: 100,
-//             height: 100
-//           }}
-//         >Info window</div>
-//       )}
-//     </React.Fragment>
-//   )
-  
-//   export default class Map extends React.Component {
-//     constructor(props) {
-//       super(props)
-//       this.state = {
-//         center: {
-//           lat: 31.2519153,
-//           lng: 34.7602093
-//         },
-//         zoom: 17,
-//         show: false
-//       }
-//     }
-  
-//     _onChildClick = (key, childProps) => {
-//       this.setState({show: !this.state.show})
-//     }
-  
-//     render() {
-//       return (
-//         <div style={{height: "100vh", width: "100%"}}>
-//           <GoogleMapReact
-//             bootstrapURLKeys={{key: 'AIzaSyBVtHhDa2ErB1UuzEc1t3dXozBGaSHiQZk'}}
-//             defaultCenter={this.state.center}
-//             defaultZoom={this.state.zoom}
-//             onChildClick={this._onChildClick}>
-//             <Marker
-//               lat={31.2519153}
-//               lng={34.7602093}
-//               show={this.state.show}
-//             />
-//           </GoogleMapReact>
-//         </div>
-//       )
-//     }
-//   }
